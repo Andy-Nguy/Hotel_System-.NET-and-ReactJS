fc72276a2c92c4b8318abe797fb162987b330e33
Andy-Nguy <duyanh05012004@gmail.com>
2025-10-17 08:25:40 +0700

    fix

--- Hotel_System/Backend/Hotel_System.API/obj/Debug/net8.0/Hotel_System.API.AssemblyInfo.cs
+++ Hotel_System/Backend/Hotel_System.API/obj/Debug/net8.0/Hotel_System.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hotel_System.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57927b618d2f8ac22c2cfbc6f117f1fdbc457c89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hotel_System.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hotel_System.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
